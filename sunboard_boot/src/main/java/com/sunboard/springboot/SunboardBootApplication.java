1ba27566c1c0fee0d312b17ea8e5848b41d90a97
Sunhee <sh089286@naver.com>
2021-10-09 22:50:15 +0900

    211009_스프링부트게시판

---
+++ sunboard_boot/src/main/java/com/sunboard/springboot/SunboardBootApplication.java
@@ -0,0 +1,13 @@
+package com.sunboard.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunboardBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunboardBootApplication.class, args);
+	}
+
+}
---
+++ sunboard_boot/src/test/java/com/sunboard/springboot/SunboardBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunboard.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunboardBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
